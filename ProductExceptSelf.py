e6d39cfe17eff14cd5e2aaaa12f17df7484e6b04
Kim Long Ngo <ngokimlong2007@gmail.com>
2025-11-20 18:44:58 -0500

    Product Except Self

---
+++ ProductExceptSelf.py
@@ -0,0 +1,28 @@
+
+def product_except_self(nums):
+    prefix = 1
+    suffix = 1
+
+    # result = []
+    # for _ in nums:
+    #     result.append(1)
+
+    result = [1] * len(nums)
+    
+    #pass 1
+    for i, num in enumerate(nums):
+        result[i] = prefix
+        prefix *= num
+    
+    #pass 2
+    index = len(nums) - 1
+    while index >= 0:
+        result[index] *= suffix
+        suffix *= nums[index]
+        index -= 1
+
+    return result
+
+print(product_except_self([4,2,8,3]))
+print(product_except_self([1,2,3,4]))
+print(product_except_self([-1,1,0,-3,3]))
